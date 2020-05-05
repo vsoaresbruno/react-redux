675b6c7c5a31e581c059aabb6f4d401bcad258dd
Bruno Soares <bruno@stoodi.com.br>
2020-05-05 16:40:57 -0300

    feat:create manage course page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./404";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/manageCoursePage";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/about' component={AboutPage} />
         <Route exact path='/courses' component={CoursesPage} />
+        <Route exact path='/course/:slug' component={ManageCoursePage} />
+        <Route exact path='/course' component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
